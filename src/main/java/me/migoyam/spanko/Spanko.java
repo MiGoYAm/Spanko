9c096fd0c9e641c51aaa28ea07b62190f3ccd7fd
MiGoYAm <miroslawjakiela@gmail.com>
2021-06-24 15:20:16 +0200

    1.17 and small fixes

--- src/main/java/me/migoyam/spanko/Spanko.java
+++ src/main/java/me/migoyam/spanko/Spanko.java
@@ -1,9 +1,8 @@
 package me.migoyam.spanko;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.boss.BarColor;
@@ -13,46 +12,42 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Pose;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerBedLeaveEvent;
-import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 public final class Spanko extends JavaPlugin implements Listener {
-    private final BossBar nightBar = Bukkit.createBossBar(null, BarColor.PURPLE, BarStyle.SEGMENTED_12), dayBar = Bukkit.createBossBar("§eJest już dzień.", BarColor.YELLOW, BarStyle.SOLID);
+    private final BossBar nightBar = getServer().createBossBar(null, BarColor.PURPLE, BarStyle.SEGMENTED_12);
+    private final BossBar dayBar = getServer().createBossBar("§eJest już dzień.", BarColor.YELLOW, BarStyle.SOLID);
     private BukkitTask task;
-    private final int dzielnik = 100/50;
+    private final int percentage = 50;
+    private final int dzielnik = 100/percentage;
 
     private int required(Player player) {
-        return (int) Math.ceil((double) player.getWorld().getPlayers().size() / dzielnik);
+        return (int) Math.ceil((double) player.getWorld().getPlayerCount()/dzielnik);
     }
     private int sleeping(Player player) {
-        List<Player> sleeping = player.getWorld().getPlayers().stream().filter(player1 -> player.getPose() == Pose.SLEEPING).collect(Collectors.toList());
-		// I stole this code from https://github.com/nkomarn/Harbor/blob/f27335e2420a331100978efe96d6b3257353a578/src/main/java/xyz/nkomarn/harbor/task/Checker.java#L160
-        return sleeping.size();
+        return (int) player.getWorld().getPlayers().stream().filter(player1 -> player1.getPose() == Pose.SLEEPING).count(); // I stole this code from https://github.com/nkomarn/Harbor/blob/f27335e2420a331100978efe96d6b3257353a578/src/main/java/xyz/nkomarn/harbor/task/Checker.java#L160
     }
     private void update(Integer sleeping, Integer required) {
         nightBar.setTitle(sleeping + "" + ChatColor.of("#7417bf") +  " z §r" + required + "" + ChatColor.of("#7417bf") + " jest w lóżku.");
-        if (sleeping >= required)
-            nightBar.setProgress(1);
-        else
-            nightBar.setProgress((double)sleeping/required);
+        nightBar.setProgress((sleeping >= required) ? 1 : (double)sleeping/required);
     }
     private void check(Player player, Integer sleeping, Integer required) {
         update(sleeping, required);
         if (sleeping == 0)
             nightBar.setVisible(false);
         if (required > sleeping && player.getWorld().getTime() != 0 && task != null)
-            Bukkit.getScheduler().cancelTask(task.getTaskId());
+            getServer().getScheduler().cancelTask(task.getTaskId());
     }
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this,this);
         nightBar.setVisible(false);
         dayBar.setVisible(false);
+
+        for(World world : getServer().getWorlds().stream().filter(world -> world.getEnvironment().equals(World.Environment.NORMAL)).collect(Collectors.toList()))
+            world.setGameRule(GameRule.PLAYERS_SLEEPING_PERCENTAGE, percentage);
     }
     @EventHandler
     public void onChangeWorld(PlayerChangedWorldEvent event) {
@@ -62,10 +57,10 @@ public final class Spanko extends JavaPlugin implements Listener {
             nightBar.addPlayer(player);
             dayBar.addPlayer(player);
         } else {
-            List<Player> sleepers = event.getFrom().getPlayers().stream().filter(player1 -> player.getPose() == Pose.SLEEPING).collect(Collectors.toList());
             nightBar.removePlayer(player);
             dayBar.removePlayer(player);
-            update(sleepers.size(), (int)Math.ceil((double) event.getFrom().getPlayers().size()/dzielnik));
+            check(player, (int) event.getFrom().getPlayers().stream().filter(player1 -> player1.getPose() == Pose.SLEEPING).count(),
+                    (int)Math.ceil((double) event.getFrom().getPlayerCount()/dzielnik));
         }
     }
     @EventHandler
@@ -80,7 +75,7 @@ public final class Spanko extends JavaPlugin implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        check(player, sleeping(player), required(player));
+        getServer().getScheduler().runTaskLater(this, ()-> check(player, sleeping(player), required(player)),1);
     }
     @EventHandler
     public void onSleep(PlayerBedEnterEvent event) {
@@ -88,27 +83,24 @@ public final class Spanko extends JavaPlugin implements Listener {
             return;
 
         Player player = event.getPlayer();
-        Bukkit.getServer().getScheduler().runTaskLater(this, ()-> {
+        getServer().getScheduler().runTaskLater(this, ()-> {
             update(sleeping(player), required(player));
             nightBar.setVisible(true);
             if (sleeping(player) >= required(player)) {
-                task = Bukkit.getScheduler().runTaskLater(this, ()-> {
+                task = getServer().getScheduler().runTaskLater(this, ()-> {
                     nightBar.setVisible(false);
-                    player.getWorld().setTime(0);
                     dayBar.setVisible(true);
                     for (Player p : player.getWorld().getPlayers())
                         p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BIT, 1, 1);
 
-                    Bukkit.getScheduler().runTaskLater(this, ()-> dayBar.setVisible(false),100);
+                    getServer().getScheduler().runTaskLater(this, ()-> dayBar.setVisible(false),100);
                 },100);
             }
         },1);
     }
     @EventHandler
     public void onWake(PlayerBedLeaveEvent event) {
-        Bukkit.getScheduler().runTaskLater(this, ()-> {
-            Player player = event.getPlayer();
-            check(player, sleeping(player), required(player));
-        },1);
+        Player player = event.getPlayer();
+        getServer().getScheduler().runTaskLater(this, ()-> check(player, sleeping(player), required(player)),1);
     }
 }
